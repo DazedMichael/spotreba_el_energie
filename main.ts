8ce373c4968cc92273b2558ead4eb2a15991a619
DazedMichael <147390720+DazedMichael@users.noreply.github.com>
2024-02-16 11:33:31 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+type Odber = {
+    cas: number; //milisekundy
+    prikon: number; //watty
+}
+let data: Array<Odber> = [
+    {cas:100, prikon:35000}, //kilowatt = 6,7kc
+    {cas:200, prikon:40000},
+    {cas:300, prikon:45000},
+    {cas:400, prikon:50000},
+    {cas:500, prikon:55000}
+]
+for (let i = 0; i <data.length; i++) {
+
+
+}
